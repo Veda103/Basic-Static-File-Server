b671363ec625090e5aca18e6ae30393d811ed5d0
Veda103 <152360821+Veda103@users.noreply.github.com>
2025-04-07 12:15:09 +0530

    Create mimeTypes.js

---
+++ modules/mimeTypes.js
@@ -0,0 +1,12 @@
+// modules/mimeTypes.js
+const mimeTypes = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.png': 'image/png',
+    '.js': 'text/javascript',
+    '.ico': 'image/x-icon'
+};
+
+module.exports = mimeTypes;
